5de99ec597c81b6ded212511d94eb294d20c1771
gabrieltkgp <gabrieltkgp@gmail.com>
2016-01-10 23:21:14 -0200

    rest working

--- FloripaPublicTransportation/app/src/main/java/com/arctouch/floripapublictransportation/ListActivity.java
+++ FloripaPublicTransportation/app/src/main/java/com/arctouch/floripapublictransportation/ListActivity.java
@@ -1,8 +1,16 @@
 package com.arctouch.floripapublictransportation;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -12,13 +20,34 @@ import android.widget.Toast;
 import com.arctouch.floripapublictransportation.classes.ItemListViewResult;
 import com.arctouch.floripapublictransportation.classes.ListViewResultAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ListView listView;
     private ListViewResultAdapter listViewResultAdapter;
     private ArrayList<ItemListViewResult> items;
+
+    private String user = "WKD4N7YMA1uiM8V";
+    private String password = "DtdTtzMLQlA0hk2C1Yi5pLyVIlAQ68";
+    private String urlRest = "https://api.appglu.com/v1/queries/findRoutesByStopName/run";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +58,7 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
 
         listView.setOnItemClickListener(this);
 
-        createListView();
+        //createListView();
     }
 
     private void createListView() {
@@ -54,5 +83,140 @@ public class ListActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         ItemListViewResult item = (ItemListViewResult) listViewResultAdapter.getItem(position);
         Toast.makeText(this, "Click: " + item.getText(), Toast.LENGTH_LONG).show();
+    }
+
+    public void onButtonClick(View v){
+        Toast.makeText(this, "Calling rest ", Toast.LENGTH_LONG).show();
+
+        //FindRouteRest findRouteRest = new FindRouteRest();
+
+        //executePost();
+        executeTestNetwork();
+
+        //Toast.makeText(this, jsonReturn, Toast.LENGTH_LONG).show();
+    }
+
+    public void executeTestNetwork(){
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            //Toast.makeText(this, "true", Toast.LENGTH_LONG).show();
+            new FindRoutNetworkRest().execute(urlRest);
+        } else {
+            Toast.makeText(this, "No network connection available.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private class FindRoutNetworkRest extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        @Override
+        protected void onPostExecute(String res) {
+            //videos = new ArrayList<Video>();
+
+            try {
+                JSONObject json = new JSONObject(res);
+                JSONArray rows = json.getJSONArray("rows");
+
+                items = new ArrayList<ItemListViewResult>();
+
+                for (int i = 0; i < rows.length(); i++) {
+                    JSONObject obj = rows.getJSONObject(i);
+
+                    ItemListViewResult item = new ItemListViewResult(obj.getString("longName"));
+
+                    items.add(item);
+
+                }
+
+                listViewResultAdapter = new ListViewResultAdapter(ListActivity.this, items);
+
+                listView.setAdapter(listViewResultAdapter);
+
+                listView.setCacheColorHint(Color.TRANSPARENT);
+
+            } catch (JSONException e) {
+                // handle exception
+            }
+        }
+
+        private String getJson(){
+            return "{ \"params\": { \"stopName\": \"%lauro linhares%\" } }";
+
+        }
+
+        private String getAuthorization(){
+            String usernamePassword = user + ":" + password;
+            String basicAuth = "Basic " + Base64.encodeToString(usernamePassword.getBytes(), Base64.NO_WRAP);
+            //String basicAuth = "Basic " + usernamePassword;
+
+            return basicAuth;
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            URL url;
+            HttpURLConnection connection = null;
+
+            try {
+                url = new URL(myurl);
+
+                String param = getJson();
+
+                connection = (HttpURLConnection)url.openConnection();
+
+                connection.setRequestMethod("POST");
+//            connection.setFixedLengthStreamingMode(param.getBytes().length);
+//            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                connection.setRequestProperty("Content-Language", "en-US");
+                connection.setRequestProperty("Authorization", getAuthorization());
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("X-AppGlu-Environment", "staging");
+
+                connection.setDoOutput(true);
+
+                DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
+
+                wr.writeBytes (param);
+                wr.flush();
+                wr.close();
+
+                is = connection.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                String line;
+                StringBuffer response = new StringBuffer();
+                while((line = rd.readLine()) != null) {
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+
+                Log.i("json",  response.toString());
+
+                return response.toString();
+
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+
+                if(connection != null) {
+                    connection.disconnect();
+                }
+            }
+        }
     }
 }
--- FloripaPublicTransportation/app/src/main/java/com/arctouch/floripapublictransportation/classes/FindRouteRest.java
+++ FloripaPublicTransportation/app/src/main/java/com/arctouch/floripapublictransportation/classes/FindRouteRest.java
@@ -1,65 +1,78 @@
 package com.arctouch.floripapublictransportation.classes;
 
+import android.os.AsyncTask;
 import android.util.Base64;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  * Created by GabrielPacheco on 05/01/2016.
  */
-public class FindRouteRest {
+public class FindRouteRest{
 
     private String user = "WKD4N7YMA1uiM8V";
     private String password = "DtdTtzMLQlA0hk2C1Yi5pLyVIlAQ68";
     private String header =  "X-AppGlu-Environment: staging";
+    private String urlRest = "https://api.appglu.com/v1/queries/findRoutesByStopName/run";
 
-    public String excutePost(String targetURL, String urlParameters)
+    public String executePost()
     {
         URL url;
         HttpURLConnection connection = null;
         try {
             //Create connection
-            url = new URL(targetURL);
+            url = new URL(urlRest);
+
+            String param="stopName=" + URLEncoder.encode("%lauro linhares%","UTF-8");
+
             connection = (HttpURLConnection)url.openConnection();
+            connection.setDoOutput(true);
             connection.setRequestMethod("POST");
+            connection.setFixedLengthStreamingMode(param.getBytes().length);
             connection.setRequestProperty("Content-Type", header);
-            String usernamePassword = user + ":" + password;
-            String basicAuth = "Basic " + Base64.encodeToString(usernamePassword.getBytes(), Base64.NO_WRAP);
-            connection.setRequestProperty("Authorization", basicAuth);
+            connection.setRequestProperty("Authorization", getAuthorization());
+
+            PrintWriter out = new PrintWriter(connection.getOutputStream());
+            out.print(param);
+            out.close();
+
+            String response= "";
+            Scanner inStream = new Scanner(connection.getInputStream());
+
+            while(inStream.hasNextLine())
+                response+=(inStream.nextLine());
+
+            return response;
+
             // connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             //connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
             //connection.setRequestProperty("Content-Language", "en-US");
 
-            connection.setUseCaches(false);
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
+            //connection.setUseCaches(false);
+            //connection.setDoInput(true);
+
 
             //Send request
-            DataOutputStream wr = new DataOutputStream (
-                    connection.getOutputStream ());
-            wr.writeBytes (urlParameters);
-            wr.flush ();
-            wr.close ();
-
-            //Get Response
-            InputStream is = connection.getInputStream();
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-            String line;
-            StringBuffer response = new StringBuffer();
-            while((line = rd.readLine()) != null) {
-                response.append(line);
-                response.append('\r');
-            }
-            rd.close();
-            return response.toString();
+//            DataOutputStream wr = new DataOutputStream (
+//                    connection.getOutputStream ());
+//            wr.writeBytes (urlParameters);
+//            wr.flush ();
+//            wr.close ();
+//
+//            //Get Response
+//            InputStream is = connection.getInputStream();
+//            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+//            String line;
+//            StringBuffer response = new StringBuffer();
+//            while((line = rd.readLine()) != null) {
+//                response.append(line);
+//                response.append('\r');
+//            }
+//            rd.close();
+//            return response.toString();
 
         } catch (Exception e) {
 
@@ -67,10 +80,20 @@ public class FindRouteRest {
             return null;
 
         } finally {
-
             if(connection != null) {
                 connection.disconnect();
             }
         }
+    }
+
+//    private String getBody(){
+//        return "{ 'params': { 'stopName': '%lauro linhares%'} }";
+//    }
+
+    private String getAuthorization(){
+        String usernamePassword = user + ":" + password;
+        String basicAuth = "Basic " + Base64.encodeToString(usernamePassword.getBytes(), Base64.NO_WRAP);
+
+        return basicAuth;
     }
 }
